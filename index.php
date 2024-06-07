dfb63ff68e3c15e0b0107fc9261c76178a4ef3eb
tomycernik <cerniktomy@gmail.com>
2024-06-07 09:50:40 -0300

    cambios traidos

--- index.php
+++ index.php
@@ -1,18 +1,44 @@
 <?php
-include_once ("Configuration.php");
-include_once ("helper/SessionManager.php");
+session_start();
+include_once("Configuration.php");
+include_once("helper/SessionManager.php");
 
 $router = Configuration::getRouter();
 $sessionManager = new SessionManager();
 
-if (!isset($_SESSION['user_id'])) {
-    $_SESSION['user_id'] = 1; // hardcodeado
-}
+$controller = $_GET["controller"] ?? "";
+$action = $_GET["action"] ?? "";
 
-$controller = isset($_GET["controller"]) ? $_GET["controller"] : "" ;
-$action = isset($_GET["action"]) ? $_GET["action"] : "" ;
+// Check if the user is logged in
+if (!isset($_SESSION['userID'])) {
+
+    // If the user is not logged in, allow access only to login and signup actions
+    if (in_array($controller, ['login', 'register']) && in_array($action, ['login', 'register', 'authenticate', 'create'])) {
+        $router->route($controller, $action);
+        exit;
+    }
+
+    // Redirect to login page if trying to access other actions
+    $controller = "user";
+    $action = "login";
+} else if ($_SESSION['role'] === 'admin') {
+    // let's leave this log for now
+    echo "<script>console.log('role: ".$_SESSION['role']."');</script>";
+    // If the user is logged in as admin, allow access to all actions except login and register
+    if (in_array($controller, ['login', 'register', ""]) && $action !== "exit") {
+        $controller = "lobby";
+        $action = "";
+    }
+} else if ($_SESSION['role'] === 'user') {
+    // let's leave this log for now
+    echo "<script>console.log('role: ".$_SESSION['role']."');</script>";
+    // If the user is logged in as user, allow access to all actions except login and register
+    if (in_array($controller, ['login', 'register', ""]) && $action !== "exit") {
+        $controller = "lobby";
+        $action = "";
+    }
+}
+// log user id on the console // lets leave this log for now
+echo "<script>console.log('user id: ".$_SESSION['userID']."');</script>";
 
 $router->route($controller, $action);
-
-// index.php?controller=tours&action=get
-// tours/get
