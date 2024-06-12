a9d868f1406939f6dc49b0e40efdc25bf765734d
Mariano Sald <odioescribirel@hotmail.com>
2024-06-12 17:32:57 -0300

    cambios previos a partida funcando

--- model/PartidaModel.php
+++ model/PartidaModel.php
@@ -24,51 +24,46 @@ class PartidaModel
 
         $this->registerUserWithThatQuestion($flowValues['question']);
 
-        $presenter->render("historia", ["question" => $flowValues['question'], "answers" => $flowValues['answer']]);
+        $presenter->render("historia", ["pregunta" => $flowValues['question']['pregunta'], "answers" => $flowValues['answers'], "correct" => $flowValues['correct']]);
     }
     public function startFlowPartida(){
         $data = [];
 
-        $randomNumber = $this->generateRandomNumber();
-        $this->bringQuestionAndAnswers($randomNumber, $data);
+        $data = $this->bringQuestionAndAnswers( $data);
         $data['userHadThatQuestion'] = $this->checkUserAndQuestion($data['question']);
         return $data;
     }
-    public function generateRandomNumber(){
-        return random_int(1,50);
-    }
+
     public function checkUserAndQuestion($question){
-        $questionId = $question[0]['id'];
+        $questionId = $question['id'];
         $sessionId = $_SESSION['userID'];
         $this->query = "SELECT * FROM user_question WHERE id_user = $sessionId AND id_question= $questionId";
 
         $successful = $this->database->query($this->query);
         return $successful;
     }
-    public function bringQuestionAndAnswers($randomNumber, $data){
+    public function bringQuestionAndAnswers($data){
         $category = array_key_last($_GET);
         //por ahora no busca por categoria(AND q.category = '$category'), al generar una pregunta al azar, tenemos que colocar preguntas en tablas separadas.
         $this->query = "SELECT q.*, a.* FROM question q 
                         LEFT JOIN answer a 
                         ON q.id = a.question_id 
-                        WHERE q.id = $randomNumber  
-                        AND a.question_id = q.id";
+                        WHERE category = '$category' 
+                        ORDER BY RAND()
+                        LIMIT 1";
         $dataRaw = $this->database->query($this->query);
-        $data['question'] = array_fill(0, 3, $dataRaw[0]);
-        $data['answers'] = array_fill(2, 5, $dataRaw[0]);
+        $data['question'] = array_slice($dataRaw[0], 0, 3);
+        $answersWithKeys = array_slice($dataRaw[0], 3, 4);
+        $data['answers'] = array_values($answersWithKeys);
+        $data['correct'] = array_slice($dataRaw[0], 7, 1);
+        $_SESSION['correct'] = $data['correct'];
         return $data;
     }
 
-    public function bringAnswer($question){
-        $id = $question[0]['id'];
-        $query = "SELECT * FROM answer WHERE question_id = $id";
-        $sucessful = $this->database->query($query);
-        return $sucessful;
-    }
     public function registerUserWithThatQuestion($question){
         $query = "INSERT INTO user_question (id_user, id_question) VALUES (?, ?)";
         $stmt = $this->prepareQuery($query);
-        $stmt->bind_param("ii", $_SESSION['userID'], $question[0]['id']);
+        $stmt->bind_param("ii", $_SESSION['userID'], $question['id']);
         $successful = $this->executionSuccessful($stmt);
         return $successful;
     }
@@ -103,5 +98,9 @@ class PartidaModel
         }
         $stmt->close();
         return false;
+    }
+
+    public function checkAnswer(){
+        $answer = array_key_exists("answer", $_POST);
     }
 }
