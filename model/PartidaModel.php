0c190014b0afcdb4983bb56167708f3b1294a0b8
Mariano Sald <odioescribirel@hotmail.com>
2024-06-24 03:01:08 -0300

    cambios en preguntas pa la partida

--- model/PartidaModel.php
+++ model/PartidaModel.php
@@ -22,55 +22,97 @@ class PartidaModel
     public function handlePartida($presenter){
 
         $flowValues = $this->startFlowPartida();
-
-        while ($flowValues['userHadThatQuestion']) {
-            $flowValues = $this->startFlowPartida();
+        $questionOK = false;
+        if ($flowValues) {
+            $questionOK = $this->registerUserWithThatQuestion($flowValues['question']);
+        
+            if($questionOK){
+                $_SESSION['questionId'] = $flowValues['question']['id'];
+            }
         }
 
-        $this->registerUserWithThatQuestion($flowValues['question']);
-
-        $presenter->render(array_key_last($_GET), 
-                            ["pregunta" => $flowValues['question']['pregunta'], 
+        $presenter->render($flowValues['category'], 
+                            ["category" => $flowValues['category'],
+                            "pregunta" => $flowValues['question']['pregunta'], 
                             "answers" => $flowValues['answers'], 
                             "answerKeys" => $flowValues['answerKeys'],
-                            "correct" => $flowValues['correct']['right_answer']]);
+                            "correct" => $flowValues['correct']['right_answer'],
+                            "questionOk" => $questionOK]);
     }
 
     public function startFlowPartida(){
         $data = [];
 
         $data = $this->bringQuestionAndAnswers($data);
-        $data['userHadThatQuestion'] = $this->checkUserAndQuestion($data['question']);
+        
         return $data;
     }
 
     public function checkUserAndQuestion($question){
         $questionId = $question['id'];
         $sessionId = $_SESSION['userID'];
-        $this->query = "SELECT * FROM user_question WHERE id_user = $sessionId AND id_question= $questionId";
+        $userQuestionQuery = "SELECT uq.id_question FROM user_question uq WHERE id_user = $sessionId";
 
-        $successful = $this->database->query($this->query);
-        return $successful;
+        $dbArray = $this->database->query($userQuestionQuery);
+
+        return count($dbArray) > 0;
     }
     public function bringQuestionAndAnswers($data){
-        $category = array_key_last($_GET);
+
+        $category = array_key_first($_POST);
+        $data['category'] = $category;
         
-        $this->query = "SELECT q.*, a.* FROM question q 
-                        LEFT JOIN answer a 
-                        ON q.id = a.question_id 
-                        WHERE category = '$category' 
-                        ORDER BY RAND()
-                        LIMIT 1";
-        $dataRaw = $this->database->query($this->query);
-        $data['question'] = array_slice($dataRaw[0], 0, 3);
+        // do {
+            $query = $this->prepareBringQuestionQuery($category);
+            $dataRaw = $this->database->query($query);
+            if (count($dataRaw > 0)) {
+                # code...
+            
+            $data['question'] = array_slice($dataRaw[0], 0, 3);
+            // $this->registerQuestionIdInQuestionList($data['question']['id']);
+            // $questionWasShown = $this->checkUserAndQuestion($data['question']);
+
+        // } while ($questionWasShown);
+        
         $answersWithKeys = array_slice($dataRaw[0], 8, 4);
         $data['answerKeys'] = array_keys($answersWithKeys);
         $data['answers'] = array_values($answersWithKeys);
         $data['correct'] = array_slice($dataRaw[0], 12, 1);
         $_SESSION['correct'] = $data['correct'];
+        
+        
+
         return $data;
+        }
+        return null;
+    }
+    public function prepareBringQuestionQuery($category){
+        $sessionId = $_SESSION['userID'];
+
+        return "SELECT q.*, a.* 
+                FROM question q
+                LEFT JOIN answer a ON q.id = a.question_id
+                LEFT JOIN user_question uq ON q.id = uq.id_question
+                WHERE q.category = 'historia' 
+                AND uq.id_user != $sessionId
+                ORDER BY RAND()
+                LIMIT 1;";
     }
 
+    public function setQuestionsList(){
+        if (!isset($_SESSION['selected_questions'])) {
+            $_SESSION['selected_questions'] = [];
+            return;
+        }
+        if($_SESSION['selected_questions'] > 0 && $_SESSION['selected_questions'] < 2) {
+            return $_SESSION['selected_questions'];
+        }
+
+        return implode(',', $_SESSION['selected_questions']);
+    }
+    public function registerQuestionIdInQuestionList($selected_question_id){
+        $_SESSION['selected_questions'][] = $selected_question_id;
+    }
     public function registerUserWithThatQuestion($question){
         $query = "INSERT INTO user_question (id_user, id_question) VALUES (?, ?)";
         $stmt = $this->prepareQuery($query);
@@ -118,10 +160,9 @@ class PartidaModel
         $correctAnswer = $_SESSION['correct']['right_answer'];
         $answerGivenByUser =  array_keys($_POST);
         $optionSubstring = $answerGivenByUser != null ? substr($answerGivenByUser[0], 7) : null;
-        $quetieneesto = $answerGivenByUser != null ? strpos($answerGivenByUser[0], $correctAnswer) : null;
 
         if($correctAnswer === $optionSubstring){
-            $presenter->render("successfulAnswer");
+            $presenter->render("successfulAnswer", ["category" => array_key_first($_POST)]);
         }
         else{
             $presenter->render("failedAnswer");
@@ -129,6 +170,7 @@ class PartidaModel
     }
 
     public function continuePartida($presenter){
-        $this->handlePartida();
+        $this->handlePartida($presenter);
     }
+
 }
